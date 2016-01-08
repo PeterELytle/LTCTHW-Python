6d16b84faef870e5f9a059fac2197ef557c17a51
PeterELytle <peter.e.lytle@outlook.com>
2016-01-08 12:45:15 -0800

    Exercise 1
    Topics:
    -Creating .py files in Notepad++ and running them from the command line
    -The print command
    -Nesting quotes
    -Commenting

---
+++ ex1.py
@@ -0,0 +1,8 @@
+print "Hello World!"
+print "Hello Again."
+print "I like typing this."
+print "This is fun."
+print 'Yay! printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+#print "This is another line of text for Python to display on the screen. I will comment it out."
