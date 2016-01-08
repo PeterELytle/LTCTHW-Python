18506e61d425a898fba6d8f1b121c0094ac16756
PeterELytle <peter.e.lytle@outlook.com>
2016-01-08 12:57:40 -0800

    Exercise 9
    Topics:
    -Printing
    -Formatters
    -Nesting formatters
    -Escape sequences
    -Triple quotes

---
+++ ex9.py
@@ -0,0 +1,21 @@
+# Here's some new strange stuff, remember to type it exactly.
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+# This line defines the variable "days".
+
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+# This line defines the variable "months", using "/n" to seperate elements onto their own lines.
+
+print "Here are the days: ", days
+# This line displays some text, and the output of the variable "days".
+
+print "Here are the months: ", months
+# This line displays some text, and the output of the variable "months".
+
+print """
+There's something going on here.
+With the three double quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+"""
+# These six lines display a body of text.
