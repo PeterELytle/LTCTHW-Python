e06debd0076171ac333e2f8afef13ffac248b250
PeterELytle <peter.e.lytle@outlook.com>
2016-01-08 12:47:44 -0800

    Exercise 3
    Topics:
    -Numbers
    -Operations
    -True/False statements

---
+++ ex3.py
@@ -0,0 +1,41 @@
+print "I will now count my chickens:"
+# This line displays some text describing what the next lines will do.
+
+print "Hens", 25.00 + 30.00 / 6.00
+# This line displays a descriptor and the result of an arithmetic equation.
+
+print "Roosters", 100.00 - 25.00 * 3.00 % 4.00
+# This line displays a descriptor and the result of an arithmetic equation.
+
+print "Now I will count the eggs:"
+# This line displays some text describing what the next lines will do.
+
+print 3.00 + 2.00 + 1.00 - 5.00 + 4.00 % 2.00 - 1.00 / 4.00 + 6.00
+# This line displays the result of an arithmetic equation.
+
+print "Is it true that 3 + 2 < 5 - 7?"
+# This line displays some text describing what the next lines will do.
+
+print 3.00 + 2.00 < 5.00 - 7.00
+# This line displays the result of an arithmetic equation as a true or false statement.
+
+print "What is 3 + 2?", 3.00 + 2.00
+# This line displays some text and the result of an arithmetic equation.
+
+print "What is 5 - 7?", 5.00 - 7.00
+# This line displays some text and the result of an arithmetic equation.
+
+print "Oh, that's why its false."
+# This line displays some text.
+
+print "How about some more?"
+# This line displays some text describing what the next lines will do.
+
+print "Is it greater?", 5 > -2
+# This line displays some text, and the result of an arithmetic equation as a true or false statement.
+
+print "Is it greater or equal?", 5 >= -2
+# This line displays some text, and the result of an arithmetic equation as a true or false statement.
+
+print "Is it less or equal?", 5 <= -2
+# This line displays some text, and the result of an arithmetic equation as a true or false statement.
