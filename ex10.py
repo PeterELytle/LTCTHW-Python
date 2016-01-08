51223194eb07f4d27ed0e65c426c4d3ebc872f8d
PeterELytle <peter.e.lytle@outlook.com>
2016-01-08 12:59:49 -0800

    Exercise 10
    Topics:
    -Escape sequences
    -Variables

---
+++ ex10.py
@@ -0,0 +1,22 @@
+tabby_cat = "\tI'm tabbed in."
+# This line defines the tabby_cat variable, and uses the "\t" escape sequence to tab the output in.
+
+persian_cat = "I'm split\non a line."
+# This line defines the persian cat variable, and uses the "\n" escape sequence to seperate the output onto two lines.
+
+backslash_cat = "I'm \\ a \\ cat."
+# This line defines the backslash cat variable, and uses the "\\" escape sequence to display a backslash.
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+# These line define the fat cat variable, using the "\t" escape sequence to tab each line in.
+
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
+# These four lines display the output of each associated variable.
